a7f252ce40f9a723e62274dda1498725d7afac9d
chintp2308 <nphuongchi23@gmail.com>
2025-08-12 08:56:23 +0700

    feat: hoàn thành tính năng login-google

--- src/main/java/com/example/springboot_education/controllers/AuthController.java
+++ src/main/java/com/example/springboot_education/controllers/AuthController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot_education.dtos.AuthDTOs.GoogleLoginRequestDto;
+import com.example.springboot_education.dtos.AuthDTOs.GoogleLoginWithCredentialRequestDto;
 import com.example.springboot_education.dtos.AuthDTOs.LoginRequestDto;
 import com.example.springboot_education.dtos.AuthDTOs.LoginResponseDto;
 import com.example.springboot_education.dtos.AuthDTOs.RegisterRequestDto;
@@ -15,6 +18,7 @@ import com.example.springboot_education.dtos.AuthDTOs.RegisterResponseDto;
 import com.example.springboot_education.services.AuthService;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -26,6 +30,10 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request) throws Exception {
+          System.out.println("=== LOGIN REQUEST ===");
+    System.out.println("username: " + request.getUsername());
+    System.out.println("email: " + request.getEmail());
+    System.out.println("password: " + request.getPassword());
         LoginResponseDto result = this.authService.login(request);
         return ResponseEntity.ok(result);
     }
@@ -37,5 +45,17 @@ public class AuthController {
      @PostMapping("/logout")
     public ResponseEntity<?> logout(HttpServletRequest request) {
         return ResponseEntity.ok().body(Map.of("message", "Logout successful"));
+    }
+
+      @PostMapping("/google-login")
+    public ResponseEntity<LoginResponseDto> googleLogin(@RequestBody @Valid GoogleLoginRequestDto request) throws Exception {
+        LoginResponseDto result = this.authService.googleLogin(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/google-login-with-credential")
+    public ResponseEntity<LoginResponseDto> googleLoginWithCredential(@RequestBody @Valid GoogleLoginWithCredentialRequestDto request) throws Exception {
+        LoginResponseDto result = this.authService.googleLoginWithCredential(request);
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/com/example/springboot_education/services/AuthService.java
+++ src/main/java/com/example/springboot_education/services/AuthService.java
@@ -1,5 +1,7 @@
 package com.example.springboot_education.services;
 
+import com.example.springboot_education.dtos.AuthDTOs.GoogleLoginRequestDto;
+import com.example.springboot_education.dtos.AuthDTOs.GoogleLoginWithCredentialRequestDto;
 import com.example.springboot_education.dtos.AuthDTOs.LoginRequestDto;
 import com.example.springboot_education.dtos.AuthDTOs.LoginResponseDto;
 import com.example.springboot_education.dtos.AuthDTOs.RegisterRequestDto;
@@ -14,9 +16,17 @@ import com.example.springboot_education.repositories.UserRoleRepository;
 import com.example.springboot_education.repositories.UsersJpaRepository;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @RequiredArgsConstructor
 @Service
@@ -66,30 +76,161 @@ public class AuthService {
 
     // ✅ Đăng nhập
     public LoginResponseDto login(LoginRequestDto request) {
-    Users user = null;
+        Users user = null;
 
-    if (request.getEmail() != null && !request.getEmail().isEmpty()) {
-        user = userJpaRepository.findByEmailWithRoles(request.getEmail())
-                .orElseThrow(() -> new HttpException("Invalid email or password", HttpStatus.UNAUTHORIZED));
-    } else if (request.getUsername() != null && !request.getUsername().isEmpty()) {
-        user = userJpaRepository.findByUsernameWithRoles(request.getUsername())
-                .orElseThrow(() -> new HttpException("Invalid username or password", HttpStatus.UNAUTHORIZED));
-    } else {
-        throw new HttpException("Username or Email is required", HttpStatus.BAD_REQUEST);
+        if (request.getEmail() != null && !request.getEmail().isEmpty()) {
+            user = userJpaRepository.findByEmailWithRoles(request.getEmail())
+                    .orElseThrow(() -> new HttpException("Invalid email or password", HttpStatus.UNAUTHORIZED));
+        } else if (request.getUsername() != null && !request.getUsername().isEmpty()) {
+            user = userJpaRepository.findByUsernameWithRoles(request.getUsername())
+                    .orElseThrow(() -> new HttpException("Invalid username or password", HttpStatus.UNAUTHORIZED));
+        } else {
+            throw new HttpException("Username or Email is required", HttpStatus.BAD_REQUEST);
+        }
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new HttpException("Invalid credentials", HttpStatus.UNAUTHORIZED);
+        }
+
+        String accessToken = jwtService.generateAccessToken(user);
+
+        List<String> roles = user.getUserRoles()
+    .stream()
+    .map(ur -> ur.getRole().getName())
+    .collect(Collectors.toList());
+
+
+        return LoginResponseDto.builder()
+                .userId(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .accessToken(accessToken)
+                  .roles(roles) 
+                .build();
     }
 
-    if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-        throw new HttpException("Invalid credentials", HttpStatus.UNAUTHORIZED);
+    public LoginResponseDto googleLogin(GoogleLoginRequestDto request) {
+        Optional<Users> user = userJpaRepository.findByEmail(request.getEmail());
+        Users newUser;
+
+        if (user.isEmpty()) {
+            newUser = new Users();
+            newUser.setUsername(request.getEmail());
+            newUser.setPassword(""); // Google login không cần mật khẩu
+                newUser.setEmail(request.getEmail()); 
+
+            // Lưu user để lấy ID
+            newUser = userJpaRepository.save(newUser);
+
+            // Gán role ID = 3
+            Role role = new Role();
+            role.setId(3);
+
+            UserRoleId userRoleId = new UserRoleId(newUser.getId(), role.getId());
+            UserRole userRole = new UserRole();
+            userRole.setId(userRoleId);
+            userRole.setUser(newUser);
+            userRole.setRole(role);
+            userRole.setEnabled(true);
+
+            // Gán UserRole vào user
+            newUser.setUserRoles(List.of(userRole));
+
+            // Lưu lại user kèm roles
+            userJpaRepository.save(newUser);
+        } else {
+            newUser = user.get();
+        }
+
+        String accessToken = jwtService.generateAccessToken(newUser);
+
+        return LoginResponseDto.builder()
+                .userId(newUser.getId())
+                .username(newUser.getUsername())
+                .email(newUser.getEmail())
+                .accessToken(accessToken)
+                .build();
+
     }
 
-    String accessToken = jwtService.generateAccessToken(user);
+    public LoginResponseDto googleLoginWithCredential(GoogleLoginWithCredentialRequestDto request) {
+        // Call google API to verify the token
+        String url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + request.getCredential();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+        Map<String, Object> payload = response.getBody();
 
-    return LoginResponseDto.builder()
-            .userId(user.getId())
-            .username(user.getUsername())
-            .email(user.getEmail())
-            .accessToken(accessToken)
-            .build();
-}
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new HttpException("Invalid Google token", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Parse the response to get the email
+        String email;
+        if (payload != null && payload.containsKey("email")) {
+            email = payload.get("email").toString();
+        } else {
+            throw new HttpException("Email not found in token", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Nên kiểm tra aud = Client ID của ứng dụng để đảm bảo token hợp lệ ( // Có thể
+        // code sau ...)
+        // String aud = payload.get("aud").toString();
+        // if (!aud.equals("YOUR_CLIENT_ID")) {
+        // throw new HttpException("Invalid Google token audience",
+        // HttpStatus.UNAUTHORIZED);
+        // }
+
+        // Kiểm tra thêm: exp so với thời gian hiện tại để đảm bảo token chưa hết hạn.
+        String iss = payload.get("iss").toString();
+        if (!iss.equals("https://accounts.google.com") && !iss.equals("accounts.google.com")) {
+            throw new HttpException("Invalid Google token issuer", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Kiểm tra thêm: exp so với thời gian hiện tại để đảm bảo token chưa hết hạn.
+        long exp = Long.parseLong(payload.get("exp").toString());
+        if (exp < System.currentTimeMillis() / 1000) {
+            throw new HttpException("Google token has expired", HttpStatus.UNAUTHORIZED);
+        }
+
+        // Find the user by email
+        // If not found, create a new user with the email.
+        Optional<Users> user = this.userJpaRepository.findByEmail(email);
+
+        // Create new user if not found
+        Users newUser = user.orElseGet(() -> {
+            Users u = new Users();
+            u.setUsername(email);
+            u.setPassword("");
+            u.setEmail(email); 
+
+            // Lưu user trước để có ID
+            Users savedUser = userJpaRepository.save(u);
+
+            // Gán role ID = 3
+            Role role = new Role();
+            role.setId(3);
+
+            UserRoleId userRoleId = new UserRoleId(savedUser.getId(), role.getId());
+            UserRole userRole = new UserRole();
+            userRole.setId(userRoleId);
+            userRole.setUser(savedUser);
+            userRole.setRole(role);
+            userRole.setEnabled(true);
+
+            // Gán UserRole vào user
+            savedUser.setUserRoles(List.of(userRole));
+
+            return userJpaRepository.save(savedUser);
+        });
+
+        // Generate a new access token (with full data + roles)
+        String accessToken = jwtService.generateAccessToken(newUser);
+
+        return LoginResponseDto.builder()
+                .userId(newUser.getId())
+                .username(newUser.getUsername())
+                .accessToken(accessToken)
+                .build();
+    }
 
 }
